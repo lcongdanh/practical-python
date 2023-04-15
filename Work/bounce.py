412c96910fedf580ed65e45cc42029a515deb683
danh le <lcdanh110628@gmail.com>
2023-04-15 20:46:55 +0700

    adding bounce.py and modify mortgage.py
    add bounce.py file with some excercises and with mortgage.py file, I've modified it using f-string

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+num_of_bounces = 0
+current_height = 100
+
+
+while num_of_bounces < 10 and current_height > 0:
+    num_of_bounces = num_of_bounces + 1
+    current_height = current_height * (3/5)
+    print("Bounces:", round(num_of_bounces, 4))
+    print("Height:",round(current_height,4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,32 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month_num = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    if payment > principal:
+        payment = principal * (1+rate/12)
+        principal = 0
+        total_paid = total_paid + payment
+        month_num+=1
+
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+        month_num+=1
+
+    if month_num >= extra_payment_start_month and month_num <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
+    # print(month_num,round(total_paid,2),round(principal,2))
+
+print(f'Total paid {total_paid:0.2f}')
+print(f'Total months {month_num}')
