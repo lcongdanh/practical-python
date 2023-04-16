075711f6e06ec65e2cc0b6c91e45c112d322c995
danh le <lcdanh110628@gmail.com>
2023-04-16 20:05:53 +0700

    modify pcost.py file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    total = 0
+    for line in f:
+        share = int(line.split(',')[1])
+        price = float(line.split(',')[2].rstrip('\n'))
+        total = total + share * price
+    print('The total price is', round(total,2))
