8179b150d040b0d16d421e9849017e3184b702d5
danh le <lcdanh110628@gmail.com>
2023-04-18 16:02:27 +0700

    modify pcost.py file to exercise 1.33

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,12 +1,30 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
 
-with open('Data/portfolio.csv', 'rt') as f:
-    headers = next(f)
-    total = 0
-    for line in f:
-        share = int(line.split(',')[1])
-        price = float(line.split(',')[2].rstrip('\n'))
-        total = total + share * price
-    print('The total price is', round(total,2))
+def portfolio_cost(filename):
+    with open(filename, 'rt') as f:
+        lines = csv.reader(f)
+        headers = next(lines)
+        total = 0
+        for line in lines:
+            share_raw = line[1]
+            price_raw = line[2]
+            try:
+                share_int = int(share_raw)
+                price_float = float(price_raw)
+                total = total + share_int * price_float
+            except:
+                print('Bad line:', line)
+    return total
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    print('I\'ve come here')
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
