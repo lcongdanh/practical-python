a6591401a2b40144de6b53efa2763d437a317591
danh le <lcdanh110628@gmail.com>
2023-04-19 16:18:38 +0700

    modiful report.py to turn the data into dictionary

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,21 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        lines = csv.reader(f)
+        next(lines)
+        for line in lines:
+            holding = {
+                'name': line[0],
+                'shares': int(line[1]),
+                'price': float(line[2])
+            }
+            portfolio.append(holding)
+    return portfolio
